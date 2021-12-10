04f036c50d4451900194b3306b968ab401dd549f
gprajwalpoojari <gprajwalpoojari@gmail.com>
2021-12-10 11:33:43 -0500

    Added more functions. TODO - Calculate heuristic

--- src/vgraph_environment/include/LocalPlanner/local_planner.hpp
+++ src/vgraph_environment/include/LocalPlanner/local_planner.hpp
@@ -5,6 +5,8 @@
 #include "Robot/robot.hpp"
 #include <queue>
 
+typedef std::pair<geometry_msgs::Point, double> robot_state;
+typedef std::priority_queue<std::pair<double, robot_state>, std::vector<std::pair<double, robot_state>>, std::greater<std::pair<double, robot_state>> pq_type;
 
 class local_planner
 {
@@ -15,6 +17,13 @@ private:
     double threshold;
     double robot_length;
     Robot robot;
+     // Ackerman drive
+    std::vector<int> U_s;
+    std::vector<int> Theta;
+    double delta_t;
+    geometry_msgs::Point from;
+    geometry_msgs::Point to;
+
 
 public:
     local_planner(std::vector<geometry_msgs::Point> global_path,  std::vector<geometry_msgs::Point> vert_list , double threshold = 10 , double robot_length = 2, std::vector<std::vector<geometry_msgs::Point>> hull_verts)
@@ -22,6 +31,11 @@ public:
                 {
                     double radius = 0.5                         // radius added randomly. Need to add correct. 
                     this->robot(this->global_path[0], 0, radius);
+                    std::vector<int> this->U_s = {-1, 1};
+                    std::vector<int> this->Theta = {-15, 0, 15}; 
+                    double this->delta_t = 1;
+                    robot->center = this->global_path[0];
+                    robot->orientation = 0;
                 }
 
     double degree_to_rad(double angle_degree)
@@ -29,31 +43,30 @@ public:
         return (angle_degree*M_PI/180);
     }
 
-    // heuristic (APF)
+    double euc_dist(geometry_msgs::Point from, geometry_msgs::Point to) {
+            double x_diff = from.x - to.x;
+            double y_diff = from.y - to.y;
+            return (sqrt(pow(x_diff, 2) + pow(y_diff, 2)));
+        }
 
-    std::vector<geometry_msgs::Point> get_path(geometry_msgs::Point from, geometry_msgs::Point to)
-    {   
-        double x_curr = from.x;
-        double y_curr = from.y;
-        double theta_curr = 0;                        // Need to add the current theta of the robot.
+    double calc_heuristic(robot_state possible_state) {
 
+    }
 
-        double heuristic = 0; // Implement APF.
-        // Ackerman drive
-        std::vector<int> U_s = {-1, 1};
-        std::vector<int> Theta = {-15, 0, 15}; 
-        double delta_t = 1;
-        // std::pair<double, std::pair<geometry_msgs::Point, double>>
-        std::priority_queue<std::pair<double, std::pair<geometry_msgs::Point, double>>, std::vector<std::pair<double, std::pair<geometry_msgs::Point, double>>>,
-                             std::greater<std::pair<double, std::pair<geometry_msgs::Point, double>>> possible_states;
-        
-        for(auto u_s : U_s)
+    pq_type get_possible_states(robot_state current_state) {
+
+        pq_type possible_states;
+        double x_curr = current_state.first.x;
+        double y_curr = current_state.first.y;
+        double theta_curr = current_state.second;
+
+         for(auto u_s : this->U_s)
         {
-            for(auto theta : Theta)
+            for(auto theta : this->Theta)
             {
-                double theta_next =  theta_curr + (u_s*tan(this->degree_to_rad(theta))/this->robot_length)* delta_t ;
-                double x_next = x_curr + (us*cos(theta_next))*delta_t;
-                double y_next = y_curr + (us*sin(theta_next))*delta_t;
+                double theta_next =  theta_curr + (u_s*tan(this->degree_to_rad(theta))/this->robot_length)* this->delta_t ;
+                double x_next = x_curr + (us*cos(theta_next))*this->delta_t;
+                double y_next = y_curr + (us*sin(theta_next))*this->delta_t;
                 geometry_msgs::Point point;
                 point.x = x_next;
                 point.y = y_next;
@@ -68,14 +81,45 @@ public:
                 }
                 if (status == false)
                 {
-                    std::pair<geometry_msgs::Point, double> possible_state = std::make_pair(point, theta_next);
-                    double heuristic = this->cal_heuristic(possible_state);
+                    robot_state possible_state = std::make_pair(point, theta_next);
+                    double heuristic = this->calc_heuristic(possible_state);
                     possible_states.push(heuristic, possible_state);   // add heuristic function
                 }
                 
             }
         }
 
+        return possible_states;
+
+     }
+
+    std::vector<robot_state> get_local_path(geometry_msgs::Point from, geometry_msgs::Point to)
+    {   
+        this->from = from;
+        this->to = to;
+        double theta_curr = robot->orientation;             ///////////////////////// Need to add the current theta of the robot.
+        robot_state current_state = std::make_pair(this->from, theta_curr);
+        std::vector<robot_state> path_points;
+        path_points.push_back(current_state);
+        while (this->euc_dist(current_state.first, this->to) > 10) {
+            pq_type possible_states = this->get_possible_states(current_state);
+            std::pair<double, robot_state> heuristic_state_pair = possible_states.top();
+            robot_state next_best_state = heuristic_state_pair.second;
+            path_points.push_back(next_best_state);
+            current_state = next_best_state;
+        }
+        this->to = current_state.first;
+        return path_points;
+
+    }
+
+    std::vector<robot_state> get_path(){
+        std::vector<std::vector<robot_state>> local_paths;
+        for (int i = 0; i < this->global_path.size() - 1; i++) {
+            std::vector<robot_state> local_path = get_local_path(this->global_path[i], this->global_path[i+1]);
+            local_paths.insert(local_paths.end(), local_path.begin(), local_path.end());
+            this->global_path[i+1] = this->to;
+        }
     }
 
     
